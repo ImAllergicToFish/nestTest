dd6414c5887bf70ae58429b347f88fb9660a0609
g.yakimenko <nneeleps@gmail.com>
2021-11-26 17:18:12 +0300

    test

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -40,11 +40,14 @@ describe('AppController (e2e)', () => {
       });
   });
 
-  it('/review/create (POST) - fail', () => {
+  it('/review/create (POST) - fail', async () => {
     return request(app.getHttpServer())
       .post('/review/create')
       .send({...testDto, rating: 0})
       .expect(400)
+      .then(({body}: request.Response) => {
+        console.log(body);
+      })
   });
 
   it('/review/byProduct/:productId (GET) - success', async () => {
